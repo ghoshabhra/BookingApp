44c45af883fa7477a01c2da53574d9cba6257363
ghoshabhra <ghoshabhra1993@gmail.com>
2024-12-24 17:19:43 +0530

    Added hello world ios app

---
+++ IOS/BookingApp/BookingApp/BookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppApp.swift
+//  BookingApp
+//
+//  Created by Abhra Ghosh on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS/BookingApp/BookingAppTests/BookingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookingAppTests.swift
+//  BookingAppTests
+//
+//  Created by Abhra Ghosh on 24/12/24.
+//
+
+import Testing
+@testable import BookingApp
+
+struct BookingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
