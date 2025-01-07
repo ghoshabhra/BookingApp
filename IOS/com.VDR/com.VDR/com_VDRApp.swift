af114f6f07c77d62efa4e2ed10be82f8f5625b48
ghoshabhra <ghoshabhra1993@gmail.com>
2025-01-07 19:52:07 +0530

    Added Helloworld IOS app

---
+++ IOS/com.VDR/com.VDR/com_VDRApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_VDRApp.swift
+//  com.VDR
+//
+//  Created by Abhra Ghosh on 07/01/25.
+//
+
+import SwiftUI
+
+@main
+struct com_VDRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS/com.VDR/com.VDRTests/com_VDRTests.swift
@@ -0,0 +1,17 @@
+//
+//  com_VDRTests.swift
+//  com.VDRTests
+//
+//  Created by Abhra Ghosh on 07/01/25.
+//
+
+import Testing
+@testable import com_VDR
+
+struct com_VDRTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
